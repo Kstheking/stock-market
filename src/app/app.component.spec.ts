7b94e00738adf469804a656d1d944e9db226be72
Khushal Sahni <kstheking0@gmail.com>
2021-07-18 15:16:50 +0530

    ahem

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,59 +1,72 @@
 import { AppComponent } from "./app.component";
 import { Stock } from "./model/stock";
+import {ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
+import { StockItemComponent } from './stock/stock-item/stock-item.component';
+import { By } from '@angular/platform-browser';
 
 describe('AppComponent', () => {
 
-  it('should have stock instantiated on ngInit', () => {
-    const appComponent = new AppComponent();
-    expect(appComponent.stockObj).toBeUndefined();
-    appComponent.ngOnInit();
-    expect(appComponent.stockObj).toEqual(new Stock('Test stock company', 'TSC', 35, 30));
+  describe('No angular aware unit tests', () => {
+    it('should have stock instantiated on ngInit', () => {
+      const appComponent = new AppComponent();
+      expect(appComponent.stockObj).toBeUndefined();
+      appComponent.ngOnInit();
+      expect(appComponent.stockObj).toEqual(new Stock('Test stock company', 'TSC', 35, 30));
+    });
+  
+    it('should toggle stock favorite ', () => {
+      const appComponent = new AppComponent();
+      appComponent.ngOnInit();
+      expect(appComponent.stockObj.favorite).toBeFalsy();
+      appComponent.toggly(new Stock('Test', 'TST', 54, 55));
+      expect(appComponent.stockObj.favorite).toBeTruthy();
+      appComponent.toggly(new Stock('Test', 'TST', 54, 55));
+      expect(appComponent.stockObj.favorite).toBeFalsy();
+    });
   });
 
-  it('should toggle stock favorite ', () => {
-    const appComponent = new AppComponent();
-    appComponent.ngOnInit();
-    expect(appComponent.stockObj.favorite).toBeFalsy();
-    appComponent.toggly(new Stock('Test', 'TST', 54, 55));
-    expect(appComponent.stockObj.favorite).toBeTruthy();
-    appComponent.toggly(new Stock('Test', 'TST', 54, 55));
-    expect(appComponent.stockObj.favorite).toBeFalsy();
+  describe('Angular-aware test', ()=> {
+    let fixture: ComponentFixture<AppComponent>, component: AppComponent;
+    beforeEach(waitForAsync(() => {
+      TestBed.configureTestingModule({
+        declarations: [
+          AppComponent,
+          StockItemComponent
+        ]
+      }).compileComponents();
+    }));
+
+    beforeEach(()=> {
+      fixture = TestBed.createComponent(AppComponent);
+      component = fixture.componentInstance;
+      fixture.detectChanges();
+    });
+
+    it('should load stock with default values', ()=> {
+      const titleEl = fixture.debugElement.query(By.css("h1"));
+      expect(titleEl.nativeElement.textContent.trim()).toEqual('Welcome to stock-market!');
+
+      const nameEl = fixture.debugElement.query(By.css('.name'));
+      expect(nameEl.nativeElement.textContent).toEqual('Test stock company (TSC)');
+
+      const priceEl = fixture.debugElement.query(By.css('.price.positive'));
+      expect(priceEl.nativeElement.textContent).toEqual('$ 35');
+
+      const addToFavoriteBtnEl = fixture.debugElement.query(By.css('button'));
+      expect(addToFavoriteBtnEl).toBeDefined();
+    });
+
+    it('should toggle stock favorite correctly', ()=> {
+      expect(component.stockObj.favorite).toBeFalsy();
+      let addToFavoriteBtnEl = fixture.debugElement.query(By.css('button'));
+      expect(addToFavoriteBtnEl).toBeDefined();
+      addToFavoriteBtnEl.triggerEventHandler('click', null);
+
+      fixture.detectChanges();
+      expect(component.stockObj.favorite).toBeTruthy();
+      addToFavoriteBtnEl = fixture.debugElement.query(By.css("button"));
+      expect(addToFavoriteBtnEl).toBeNull();
+    });
   });
 
 })
-
-// import { TestBed } from '@angular/core/testing';
-// import { RouterTestingModule } from '@angular/router/testing';
-// import { AppComponent } from './app.component';
-
-// describe('AppComponent', () => {
-//   beforeEach(async () => {
-//     await TestBed.configureTestingModule({
-//       imports: [
-//         RouterTestingModule
-//       ],
-//       declarations: [
-//         AppComponent
-//       ],
-//     }).compileComponents();
-//   });
-
-//   it('should create the app', () => {
-//     const fixture = TestBed.createComponent(AppComponent);
-//     const app = fixture.componentInstance;
-//     expect(app).toBeTruthy();
-//   });
-
-//   it(`should have as title 'stock-market'`, () => {
-//     const fixture = TestBed.createComponent(AppComponent);
-//     const app = fixture.componentInstance;
-//     expect(app.title).toEqual('stock-market');
-//   });
-
-//   it('should render title', () => {
-//     const fixture = TestBed.createComponent(AppComponent);
-//     fixture.detectChanges();
-//     const compiled = fixture.nativeElement;
-//     expect(compiled.querySelector('.content span').textContent).toContain('stock-market app is running!');
-//   });
-// });
