3b891495e24943b760a141f7bc9c5180c3591dc4
Khushal Sahni <kstheking0@gmail.com>
2021-08-13 14:38:35 +0530

    ahem

--- src/app/model/stock.ts
+++ src/app/model/stock.ts
@@ -1,9 +1,15 @@
 export class Stock {
-    favorite: boolean = false;
-    constructor(public name: string, public code: string, public price: number, public previousPrice: number, public exchange: string){
-
+    favorite?: boolean = false;
+    notablePeople?: Person[];
+    constructor(public name: string, public code: string, public price: number, public previousPrice?: number, public exchange?: string){
+        this.notablePeople = [];
     }
     isPositiveChange(): boolean{
-        return this.price >= this.previousPrice;
+        return this.price >= this.previousPrice!;
     }
+}
+
+export class Person{
+    name!: string;
+    title!: string;
 }
--- src/app/stock/create-stock/create-stock.component.ts
+++ src/app/stock/create-stock/create-stock.component.ts
@@ -1,31 +1,77 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup, NgForm, Validators } from '@angular/forms';
+import { FormArray, FormBuilder, FormControl, FormGroup, NgForm, Validators } from '@angular/forms';
 import { Stock } from 'src/app/model/stock';
+
+let counter = 1;
 
 @Component({
   selector: 'app-create-stock',
   templateUrl: './create-stock.component.html',
   styleUrls: ['./create-stock.component.css']
 })
+
+
+
 export class CreateStockComponent implements OnInit {
+  public stock!: Stock;
   public stockForm!: FormGroup;
   constructor(private fb: FormBuilder) {
     this.createForm();
-   }
+    this.stock = new Stock('Test ' + counter++, 'TST', 20, 10, 'NASDAQ');
+  }
 
-   createForm(){
-     this.stockForm = this.fb.group({
-       name: [null, Validators.required],
-       code: [null, [Validators.required, Validators.minLength(2)]],
-       price: [0, [Validators.required, Validators.min(0)]]
-     })
-   }
+  createForm() {
+    this.stockForm = this.fb.group({
+      name: [null, Validators.required],
+      code: [null, [Validators.required, Validators.minLength(2)]],
+      price: [0, [Validators.required, Validators.min(0)]],
+      notablePeople: this.fb.array([])
+    })
+  }
+
+  get notablePeople(){
+    return this.stockForm.get('notablePeople') as FormArray;
+  }
+
+  addNotablePerson(){
+    this.notablePeople.push(this.fb.group({
+      name: ['', Validators.required],
+      title: ['', Validators.required]
+    }))
+  }
+
+  removeNotablePerson(index: number){
+    this.notablePeople.removeAt(index);
+  }
+
+  loadStockFromServer() {
+    this.stock = new Stock('Test ' + counter++, 'TST', 20, 10, 'BITCH');
+    let stockFormModel = Object.assign({}, this.stock);
+    delete stockFormModel.previousPrice;
+    delete stockFormModel.favorite;
+    delete stockFormModel.exchange;
+    this.stockForm.setValue(stockFormModel);
+  }
+
+  patchStockForm() {
+    this.stock = new Stock(`Test ${counter++}`, 'TST', 20, 10);
+    this.stockForm.patchValue(this.stock);
+  }
+
+  resetForm() {
+    this.stockForm.reset();
+  }
 
   ngOnInit(): void {
   }
 
-  onSubmit(){
-    console.log(this.stockForm.value);
+  onSubmit() {
+    this.stock = Object.assign({}, this.stockForm.value);
+    console.log('Saving stock', this.stock);
   }
-  
+
+  get name() {
+    return this.stockForm.get('name');
+  }
+
 }
